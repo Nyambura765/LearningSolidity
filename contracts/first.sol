7bf8225179d704254cf95369dbafa127c6d44858
Nyambura765 <nyamburafavour765@gmail.com>
2024-11-05 13:34:08 +0300

    data types

--- contracts/first.sol
+++ contracts/first.sol
@@ -10,6 +10,18 @@ contract SolidityTest {
     /**
     this performs a basic addition arithmetics
     **/
+    /** prinitive datatypes in solidity are
+    1.Boolean=true or false values
+    2.uint=unsigned integers whicha are basically non negative numbers
+      uint8
+      uint16
+      uint256
+    3.int=integer which are basically negative numbers
+       int8
+       int16
+       int256
+    4.Adress=holds the 20 byte value representing the size of an Ethereum address.
+    **/
       uint a = 1;
       uint b = 2;
       uint result = a + b;
